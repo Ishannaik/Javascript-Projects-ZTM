040eeca5c977de338e6208226d34776db61f7b4c
Ishan <ishannaik7@gmail.com>
2024-01-25 16:51:48 +0530

    Final change

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,54 +1,68 @@
-// let apiQuotes = [];
 const quoteContainer = document.getElementById('quote-container');
 const quoteText = document.getElementById('quote');
-const authorText = document.getElementById('quote-author');
+const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-//Show new Quote
-function newQuote(){
-    //Pick a random quote from apiQuotes array
-    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+let apiQuotes = [];
 
-    //Check is Author field is blank and replace it with 'Unknown'
-    if(!quote.author){
-        authorText.textContent = 'Unknown';
-    }else{
+// Loading Spinner Shown
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Remove Loading Spinner
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// Show New Quote
+function newQuote() {
+  loading();
+  // Pick a random quote from array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
     authorText.textContent = "LAW " + quote.author;
-    }
-    if (quote.text.length > 120){
-        quoteText.classList.add('long-quote');
-    }
-    else{
-        quoteText.classList.remove('long-quote');
-    }
-    quoteText.textContent = quote.text;
+  }
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  // Set Quote, Hide Loader
+  quoteText.textContent = quote.text;
+  complete();
 }
 
-//Tweet Quote
-function tweetQuote(){
-    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-    window.open(twitterUrl,'_blank');
-}
-
-//Event Listeners
-newQuoteBtn.addEventListener('click',newQuote);
-twitterBtn.addEventListener('click',tweetQuote);
-
-newQuote();
-//  Get Quotes From API
-
-// async function getQuotes(){
-//     const apiUrl = 'https://type.fit/api/quotes';
-//     try{
-//         const response = await fetch(apiUrl);
-//         apiQuotes = await response.json();
-//         newQuote();
-//     }catch(error)
-//     {
-        // Catch Error Here
-//     }
+// // Get Quotes From API
+// async function getQuotes() {
+//   loading();
+//   const apiUrl = 'https://ishannaik.github.io/quotes-api/data/quotes.json';
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {
+//     // Catch Error Here
+//   }
 // }
 
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
 // On Load
-// getQuotes();
+getQuotes();
