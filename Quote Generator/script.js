39676635cc640d7611d3878a6b454ed56a3c64e7
Ishan <ishannaik7@gmail.com>
2024-01-25 16:28:16 +0530

    Pushing to main branch

--- Quote Generator/script.js
+++ Quote Generator/script.js
@@ -1,18 +1,54 @@
-let apiQuotes = [];
+// let apiQuotes = [];
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('quote-author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
-//  Get Quotes From API
+//Show new Quote
+function newQuote(){
+    //Pick a random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
 
-async function getQuotes(){
-    const apiUrl = 'https://type.fit/api/quotes';
-    try{
-        const response = await fetch(apiUrl);
-        apiQuotes = await response.json();
-        console.log(apiQuotes[]);
-    }catch(error)
-    {
-        // Catch Error Here
+    //Check is Author field is blank and replace it with 'Unknown'
+    if(!quote.author){
+        authorText.textContent = 'Unknown';
+    }else{
+    authorText.textContent = "LAW " + quote.author;
     }
+    if (quote.text.length > 120){
+        quoteText.classList.add('long-quote');
+    }
+    else{
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
 
+//Tweet Quote
+function tweetQuote(){
+    const twitterUrl=`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl,'_blank');
+}
+
+//Event Listeners
+newQuoteBtn.addEventListener('click',newQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
+newQuote();
+//  Get Quotes From API
+
+// async function getQuotes(){
+//     const apiUrl = 'https://type.fit/api/quotes';
+//     try{
+//         const response = await fetch(apiUrl);
+//         apiQuotes = await response.json();
+//         newQuote();
+//     }catch(error)
+//     {
+        // Catch Error Here
+//     }
+// }
+
 // On Load
-getQuotes();
+// getQuotes();
